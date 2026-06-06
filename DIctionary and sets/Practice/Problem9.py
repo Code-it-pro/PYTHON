68ebc8b3bdb26ccf3a91c3ee1702e6924160474e
Gagandeep <editoranonymous47@gmail.com>
2026-06-06 17:25:41 +0530

    Update Problem9.py

--- DIctionary and sets/Practice/Problem9.py
+++ DIctionary and sets/Practice/Problem9.py
@@ -2,3 +2,4 @@ S = {8, 7 , 12, "Python", [1,2]}
 
 print(S)
 
+# Not hashable, so it cannot be added to a set. The list [1,2] is mutable and therefore not hashable, which is why it cannot be added to the set S.
