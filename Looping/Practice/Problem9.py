2aeed9a0646d628b8241dd10963ba0387fb51967
Gagandeep <editoranonymous47@gmail.com>
2026-06-08 01:22:24 +0530

    Update Problem9.py

--- Looping/Practice/Problem9.py
+++ Looping/Practice/Problem9.py
@@ -13,15 +13,24 @@ n = 3
 '''
 
 
-print("*" * n )
+# print("*" * n )
 
-for i in range(1, n+1):
-    for i in range(1, n+1):
-        if(i == 1 or i == n):
-            print("*", end="")
-        else:
-            print(" ",end="")
-    print("")
+# for i in range(1, n+1):
+#     for i in range(1, n+1):
+#         if(i == 1 or i == n):
+#             print("*", end="")
+#         else:
+#             print(" ",end="")
+#     print("")
 
     
-print("*" * n)
+# print("*" * n)
+
+for i in range(1, n+1):
+    if(i==1 or i==n):
+        print("*" * n)
+    else:
+        print("*", end="")
+        print(" " * (n-2), end="")
+        print("*")
+print("")
